fe0cfe973a81134f91c414c46a63aeb8f3dd4f5c
Mert Gunduz <merttgg@gmail.com>
2021-08-29 19:57:24 +0300

    Booleans Added

---
+++ Source/Booleans/Booleans.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+int main() 
+{
+      bool isClientHacker;
+      
+      if (isClientHacker) 
+      {
+            printf("\nClient Is A Hacker!\n");
+      }
+      else 
+      {
+            printf("\nClient Is Not A Hacker!\n");
+      }
+}
