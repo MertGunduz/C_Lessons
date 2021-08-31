c8c1d4e56b794d4aee255e2f8f749b8e2cadfeb8
Mert Gunduz <merttgg@gmail.com>
2021-08-31 19:24:43 +0300

    Unary Operators Added

---
+++ Source/Operators & Bit Manipulation/Unary Operators/UnaryOperators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+      int firstNumber, secondNumber;
+
+      printf("\n|======================================================|\n\n");
+
+      printf("First Number -> ");
+      scanf("%d", &firstNumber);
+
+      printf("Second Number -> ");
+      scanf("%d", &secondNumber);
+
+      printf("\n"); // |-| Line Breaker |-|
+
+      firstNumber++;
+      secondNumber++;
+
+      printf("First Number Unary(++) -> %d\n", firstNumber);
+      printf("Second Number Unary(++) -> %d\n\n", secondNumber);
+
+      printf("|======================================================|\n\n");
+      return EXIT_SUCCESS;
+}
