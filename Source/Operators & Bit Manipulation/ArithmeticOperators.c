c727d7e3c22e04a22a140a36c9ae91f55731dbaf
Mert Gunduz <merttgg@gmail.com>
2021-08-31 18:35:32 +0300

    Arithmetic Operators

---
+++ Source/Operators & Bit Manipulation/ArithmeticOperators.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+      int firstNumber, secondNumber, operationResult;
+
+      printf("\n|===================================================|\n\n"); // UI START
+
+      printf("First Number -> ");
+      scanf("%d", &firstNumber);
+
+      printf("Second Number -> ");
+      scanf("%d", &secondNumber);
+
+      printf("\n"); // |-| Line Breaker |-|
+
+      // Addition Operation
+      operationResult = firstNumber + secondNumber;
+      printf("firstNumber + secondNumber -> %d\n", operationResult);
+
+      // Subtraction Operation 
+      operationResult = firstNumber - secondNumber;
+      printf("firstNumber - secondNumber -> %d\n", operationResult);
+
+      // Multiplication Operation
+      operationResult = firstNumber * secondNumber;
+      printf("firstNumber * secondNumber -> %d\n", operationResult);
+
+      // Division Operation
+      operationResult = firstNumber / secondNumber;
+      printf("firstNumber / secondNumber -> %d\n", operationResult);
+
+      printf("\n|===================================================|\n"); // UI FINISH
+}
