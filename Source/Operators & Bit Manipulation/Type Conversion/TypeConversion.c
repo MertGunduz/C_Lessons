c0834f9e583b99ee4341ac7386a923c0bc3e3f95
Mert Gunduz <merttgg@gmail.com>
2021-09-01 13:57:31 +0300

    Type Conversions Added

---
+++ Source/Operators & Bit Manipulation/Type Conversion/TypeConversion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+      int integerNumber;
+      float floatNumber = 5121.312f;
+
+      integerNumber = (int)floatNumber;
+
+      printf("Result -> %d\n", integerNumber);
+}
