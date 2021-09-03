7089aa7954a68f671dc205633ebdcbb8a3336ef9
Mert Gunduz <merttgg@gmail.com>
2021-09-03 21:33:00 +0300

    Void Methods Added

---
+++ Source/Methods/Void_Methods/VoidMethods.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void DrawSword() 
+{
+      printf("      _\n");
+      printf("     /|\n");
+      printf("    //|\n");
+      printf("   // |\n");
+      printf("  //  |\n");
+      printf(" //   |\n");
+      printf("//    |\n");
+      printf("||____|\n");
+      printf("|| *  |\n");
+      printf("|| *  |\n");
+      printf("|| *  |\n");
+      printf("|| *  |\n");
+      printf("|| *  |\n");
+      printf("|| *  |\n");
+      printf("||----|\n");
+      printf("|-----|\n");
+      printf(" |---/ \n");
+      printf("  |-|  \n");
+      printf("  |_|  \n");
+}
+
+int main()
+{
+      DrawSword();
+}
