6891fda118c6cd3c1e5320e73598b38818f9e941
Mert Gunduz <merttgg@gmail.com>
2021-09-04 12:10:27 +0300

    Null Pointers Added

--- Source/Pointers/Declaring_Pointers/DeclaringPointers.c
+++ Source/Pointers/Declaring_Pointers/DeclaringPointers.c
@@ -1,12 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 int main() 
 {
-      int myNumber = 421;
-      int *pointer = &myNumber;
+      char a = 'A';
+      char *ptr = &a;
 
-      int pointedNumber = *pointer;
+      printf("\n");
+      printf("Pointed Char (A) --> %c\n", *ptr);
+      printf("Pointed Char (A) Memory Location --> %llu (RAM LOCATION)\n\n", (uint64_t) ptr);
 
-      printf("Data --> %d\n", pointedNumber);
-}
+      char b = 'B';
+      ptr = &b;
+
+      printf("Pointed Char (B) --> %c\n", *ptr);
+      printf("Pointed Char (B) Memory Location --> %llu (RAM LOCATION)\n\n", (uint64_t) ptr);
+
+      char c = 'C';
+      ptr = &c;
+
+      printf("Pointed Char (C) --> %c\n", *ptr);
+      printf("Pointed Char (C) Memory Location --> %llu (RAM LOCATION)\n\n", (uint64_t) ptr);
+}     
