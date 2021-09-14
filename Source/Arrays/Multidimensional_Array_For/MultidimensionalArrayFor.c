9f060eb9274a8293d9bc65cdab9d69b3d8f17d8d
Mert Gunduz <merttgg@gmail.com>
2021-09-14 13:18:49 +0300

    Multidimensional Array For Loop

---
+++ Source/Arrays/Multidimensional_Array_For/MultidimensionalArrayFor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ARRAY[3][3] =
+    {
+        {3, 6, 9},
+        {12, 15, 18},
+        {21, 24, 27},
+        {30, 33, 36},
+    };
+
+    for (int i = 0; i < 3; i++ )
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            printf("%d ", ARRAY[i][j]);
+            if (j == 2)
+            {
+                printf("\n");
+            }
+        }
+    }
+
+    return 0;
+}
